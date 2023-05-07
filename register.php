4d744673cd1ebcca0d8a8a2d8638496572216b66
Elahira <thusieusat@gmail.com>
2023-05-07 14:02:42 +0700

    change

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php require_once('inc/top.php') ?>
+<!-- Site title -->
+<title>Galio - Mega Shop Responsive Bootstrap 4 Template</title>
+</head>
+
+<body>
+
+    <!-- color switcher start -->
+    <?php require_once('inc/color-switcher.php') ?>
+
+    <!-- color switcher end -->
+
+    <div class="wrapper box-layout">
+
+        <!-- header area start -->
+        <?php require_once('inc/header.php') ?>
+        <!-- header area end -->
+
+        <!-- row start -->
+        
+        <!-- row end -->
+
+
+        <!-- footer area start -->
+        <?php require_once('inc/footer.php') ?>
